6c30c718543dd1e41a8f0274cad9a0a92cceb49b
vanessavtr <vanessavtr7@gmail.com>
2026-03-06 18:44:39 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("primeiro commit");
+let nome = "Luffy";
+let xp = 7500;
+let nivel = "";
+
+switch (true) {
+    case (xp < 1000):
+        nivel = "Ferro";
+        break;
+
+    case (xp <= 2000):
+        nivel = "Bronze";
+        break;
+
+    case (xp <= 5000):
+        nivel = "Prata";
+        break;
+
+    case (xp <= 7000):
+        nivel = "Ouro";
+        break;
+
+    case (xp <= 8000):
+        nivel = "Platina";
+        break;
+
+    case (xp <= 9000):
+        nivel = "Ascendente";
+        break;
+
+    case (xp <= 10000):
+        nivel = "Imortal";
+        break;
+
+    default:
+        nivel = "Radiante";
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel);
